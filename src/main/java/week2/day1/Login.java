316630cb01230836c224a52aa6d0fb48d711b428
kannavignesh <rickthetester@outlook.com>
2025-04-10 15:25:12 +1200

    changes

--- src/main/java/week2/day1/Login.java
+++ src/main/java/week2/day1/Login.java
@@ -1,9 +1,16 @@
 package week2.day1;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Login {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("");
 
 	}
 
